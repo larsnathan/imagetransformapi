3379e08827325cec6fc3d8bbdfbeca071dfa652e
Nathan Larson <larsnathanson@gmail.com>
2016-07-11 12:27:21 -0700

    Changed method to GET and added puts

--- app/controllers/transform_controller.rb
+++ app/controllers/transform_controller.rb
@@ -12,35 +12,48 @@ class TransformController < ApplicationController
   end
 
   def create
-    img1_url = params[:picture1]
-    open('image1.jpg', 'wb') do |file|
-      file << open(img1_url).read
-    end
-
-    img2_url = params[:picture2]
-    open('image2.jpg', 'wb') do |file|
-      file << open(img2_url).read
-    end
-
-    first_image = MiniMagick::Image.new("image1.jpg")
-    second_image = MiniMagick::Image.new("image2.jpg")
-
-    result = first_image.composite(second_image) do |c|
-      c.compose "Over"    # OverCompositeOp
-      c.geometry "+20+20" # copy second_image onto first_image from (20, 20)
-    end
-    result.write "output.jpg"
-    # MiniMagick::Tool::Mogrify.new do |mogrify|
-    #   mogrify.blur("5x3")
-    #   mogrify << "image.jpg"
+    # img1_url = params[:picture1]
+    # open('image1.jpg', 'wb') do |file|
+    #   file << open(img1_url).read
     # end
+
+    # img2_url = params[:picture2]
+    # open('image2.jpg', 'wb') do |file|
+    #   file << open(img2_url).read
+    # end
+
+    # first_image = MiniMagick::Image.new("image1.jpg")
+    # second_image = MiniMagick::Image.new("image2.jpg")
+
+    # result = first_image.composite(second_image) do |c|
+    #   c.compose "Over"    # OverCompositeOp
+    #   c.geometry "+20+20" # copy second_image onto first_image from (20, 20)
+    # end
+    # result.write "output.jpg"
+
+    image_url = params[:picture]
+    open('image.jpg', 'wb') do |file|
+      file << open(image_url).read
+    end
+
+    puts "Got image from URL"
+
+    MiniMagick::Tool::Mogrify.new do |mogrify|
+      mogrify.blur("5x3")
+      mogrify << "image.jpg"
+    end
+
+    puts "Transformed Image"
 
     auth = {
       cloud_name: "natekronos",
       api_key: "529551768137712",
       api_secret: "86j1wf0r8JgITaKKdxgLDMnLNWY"
     }
-    image_hash = Cloudinary::Uploader.upload('output.jpg', auth)
+    image_hash = Cloudinary::Uploader.upload('image.jpg', auth)
+
+    puts "uploaded image to Cloudinary"
+
     image_url = image_hash["url"]
     render json: { :image_url => image_url }
   end
